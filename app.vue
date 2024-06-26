c27fe2ec550e68dff4bff1d8691f26739d720e6a
Leonel Pabst <leonel.pabst@singular-it.de>
2024-06-26 12:28:05 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <n-avatar
+      round
+      size="small"
+      src="nonexistent.png"
+    >
+      <template #fallback>
+        Me
+      </template>
+    </n-avatar>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NAvatar } from 'naive-ui';
+</script>
